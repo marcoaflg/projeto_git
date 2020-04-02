d7bd749e10f29b6043cd3ed5964259c67037ba29
Marco Garcia <marcosl19@gmail.com>
2020-04-02 00:08:42 -0300

    navbar concerté et amélioré

--- integrador/home-2.0/js/custom.js
+++ integrador/home-2.0/js/custom.js
@@ -5,6 +5,17 @@ $('.slider').slider({
     interval: 3000
 });
 
+$(document).ready(function(){                 
+    $('input.autocomplete').autocomplete({
+    data: {
+        "Musica": null,
+        "Tecnologia": null,
+        "Sociedada": null,
+        "Temas":null
+    }
+    });                
+    });
+
 $('#nextBtn').click(function(){
     $('.slider').slider('start');
     setTimeout(function(){
