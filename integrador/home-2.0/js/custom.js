4959432c9fce839e9cba9f3befa5262b398a7c69
Marco Garcia <marcosl19@gmail.com>
2020-04-06 03:18:37 -0300

    add comments and logo link

--- integrador/home-2.0/js/custom.js
+++ integrador/home-2.0/js/custom.js
@@ -1,24 +1,31 @@
+//Tooltip - Bottom description ticket 
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
+  
+// Hamburguer Down Menu 
 $('.dropdown-trigger').dropdown();
 
+// Home Main Slider
 $('.slider').slider({
     full_width: true,
     interval: 3000
 });
-
-$(document).ready(function(){                 
-    $('input.autocomplete').autocomplete({
-    data: {
-        "Musica": null,
-        "Tecnologia": null,
-        "Sociedada": null,
-        "Temas":null
-    }
-    });                
-    });
-
-$('#nextBtn').click(function(){
+$('#nextBtn').click(function () {
     $('.slider').slider('start');
-    setTimeout(function(){
+    setTimeout(function () {
         $('.slider').slider('pause');
-    },800);
+    }, 800);
+});
+
+// Autocomplete Search Bar
+$(document).ready(function () {
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Musica": null,
+            "Tecnologia": null,
+            "Sociedada": null,
+            "Temas": null
+        }
+    });
 });
